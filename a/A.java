eaa688abc0e244f72cc277f306457d7ef32fa4c7
Nils Reiter <nils.reiter@uni-koeln.de>
2024-04-17 09:58:21 +0200

    reference solution for a)

--- a/A.java
+++ a/A.java
@@ -3,20 +3,21 @@ package a;
 import java.util.Random;
 
 public class A {
-	public static void (String args) {
-		int s = "Hello World!";
+	public static void main (String args) {
+		String s = "Hello World!";
 		char c = 'X';
-		int[] numberList = int[15];
-		for (int i; i < numberList.length; i++) {
+		int[] numberList = new int[15];
+		for (int i = 0; i < numberList.length; i++) {
+		    	Random random = new Random();
 			numberList[i] = random.nextInt(100);
 		}
 		
-		sum = 0;
+		int sum = 0;
 		for (int i = 0; i < numberList.length; c++) {
 			sum += numberList[i];
 		}
-		if sum > 750 {
-			println(s);
+		if (sum > 750) {
+			System.out.println(s);
 		}
 	}
 }
